af30153b0955d2ef60d9219a7725a4260ff0e192
kosta <kosta@kosta-PC>
2019-09-18 16:54:57 +0900

    16:54

--- src/kosta/mission/Mission05.java
+++ src/kosta/mission/Mission05.java
@@ -4,6 +4,56 @@ import java.util.Scanner;
 
 public class Mission05 {
 
+	
+	/*	public static void totalArr(int[] arr) {
+			
+			arr[3] = arr[0] + arr[1] + arr[2];
+			
+			
+		}
+		
+		public static void averageArr(int[] arr) {
+			
+			arr[4] = arr[3] / 3;
+			
+		}
+		
+		*/
+	
+	
+	public static void input(int[] arr, String[] subject) {
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i = 0 ; i < 3 ; i++) {
+			System.out.print(subject[i]+" 성적을 입력하세요 : ");
+			arr[i] = sc.nextInt();
+			
+			System.out.println(arr[i]);
+			
+			arr[3] += arr[i];
+		}
+		
+		arr[4] = arr[3] / 3;
+	}
+	
+	
+	public static void output(int[] arr, String[] subject) {
+		
+		for(String s : subject) {
+			System.out.print(s + "\t");
+		}
+		System.out.println();
+		
+		for(int n : arr) {
+			System.out.print(n + "\t");
+		}
+		System.out.println();
+		
+		
+		
+	}
+	
+	
 	public static void main(String[] args) {
 		// 성적관리 프로그램을 구현하자
 		// 국어, 영어, 수학 점수 입력 받을 수 있는 변수를 선언
@@ -13,13 +63,13 @@ public class Mission05 {
 		// 배열을 이용하여 구현해 보자
 		
 		
-		Scanner sc = new Scanner(System.in);
+		
 		
 		
 		int arr[] = new int[5];
-		String subject[] = {"국어", "영어", "수학"};
+		String subject[] = {"국어", "영어", "수학", "평균", "총점"};
 	
-			System.out.print("국어 성적을 입력 하세요 : " );
+		/*	System.out.print("국어 성적을 입력 하세요 : " );
 			arr[0] = sc.nextInt();
 			
 			//System.out.println(arr[0]);
@@ -28,28 +78,32 @@ public class Mission05 {
 			arr[1] = sc.nextInt();
 			
 			System.out.print("수학 성적을 입력 하세요 : " );
-			arr[2] = sc.nextInt();
+			arr[2] = sc.nextInt();*/
 		
 		
-		/*		for(int i = 0 ; i < 3 ; i++) {
-					System.out.print(subject[i]+" 성적을 입력하세요 : ");
-					arr[i] = sc.nextInt();
-					
-					System.out.println(arr[i]);
-					
-					arr[3] += arr[i];
-				}
+		/*		
 				*/
 		
-		arr[3] = arr[0] + arr[1] + arr[2] ;
-		System.out.println("arr[3]의 값 = " + arr[3]);
-		
-		
-		
-		arr[4] = arr[3]/3;
+		/*	arr[3] = arr[0] + arr[1] + arr[2] ;
+			System.out.println("arr[3]의 값 = " + arr[3]);
+			
+			
+			
+			arr[4] = arr[3]/3;*/
+			
+		/*	totalArr(arr);
+			averageArr(arr);
 		
 		System.out.println("총점 : " + arr[3]);
 		System.out.println("평균 : " + arr[4]);
+		
+		
+		*/
+		
+		input(arr, subject);
+		output(arr, subject);
+		
+		
 		
 		
 		
