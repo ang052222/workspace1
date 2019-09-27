6efbdd8d839af0f229f1205c990658b4c3bfb567
ang052222 <ang052222@gmail.com>
2019-09-27 17:49:03 +0900

    게임 숙제중

---
+++ src/kosta/game/Game.java
@@ -0,0 +1,30 @@
+package kosta.game;
+
+public class Game {
+
+	private int userNum;
+	private RandomNumber rn[];
+	
+	
+	
+	
+	public Game() {
+		
+		rn = new RandomNumber[3]; 
+		
+	}
+	
+	public void randomInput() {
+		
+		for(int i = 0 ; i < rn.length ; i++) {
+			//rn[i] = RandomNumber.r.nextInt(10);
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+}
