cddce7c76be496a37c2eabc4ce48bf2b3bb7fef9
ang052222 <ang052222@gmail.com>
2019-10-02 17:45:02 +0900

    과제,,따흐흑,,

--- src/kosta/io/CopyUtil.java
+++ src/kosta/io/CopyUtil.java
@@ -1,6 +1,9 @@
 package kosta.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 
 public class CopyUtil {
 	
@@ -16,7 +19,32 @@ public class CopyUtil {
 		// 재귀함수를 써서 그 디렉토리 파일안에 파일들은 복사하고 디렉토리는 파일 생성 --반복
 		
 		
+		File arr[] = f.listFiles();
 		
+		for(int i = 0 ; i < arr.length ; i++) {
+			
+			String name = arr[i].getName();
+			
+			
+			if(arr[i].isDirectory()) {
+				// 파일이름 똑같게 복사해주어야 한다. 
+				File df = new File(dest, arr[i].getName());
+				df.mkdir();
+				
+				
+			}else if(arr[i].isFile()) {
+				// copyFile()로 복사해와야한다.
+				
+				
+				
+			}else {
+				System.out.println("★★★★★★★★★★★else야★★★★★★★★★★★★★★");
+			}
+			
+			
+			
+			
+		}
 		
 		
 		
@@ -35,6 +63,55 @@ public class CopyUtil {
 	// ByteStream
 	// 바이트 값을 가진 source(파일) → dest(파일) 복사
 	public static void copyFile(File source, File dest) throws Exception{
+		
+		
+		FileInputStream  fis = null;
+		FileOutputStream fos = null;
+		
+		
+		try {
+			
+			fis = new FileInputStream(source);
+			fos = new FileOutputStream(dest);
+			
+			int i = 0;
+			
+			//while((i = fis))
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 	}
